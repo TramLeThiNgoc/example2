58d23af599e70eeab65004da076db1544aab8752
Unknown <t6leth00@students.oamk.fi>
2017-03-20 09:20:10 +0200

    update button work

--- customers.php
+++ customers.php
@@ -1,18 +1,36 @@
+<?php
+include "connection.php";
+
+if(isset($_POST['btnAdd'])){
+  $add=$db->prepare("INSERT INTO customers (firstname,lastname,streetaddress) VALUES(:fn,:ln,:ad)");
+  $add->bindParam(':fn',$fn);
+  $add->bindParam(':ln',$ln);
+  $add->bindParam(':ad',$ad);
+  $fn=$_POST['fn'];
+  $ln=$_POST['ln'];
+  $ad=$_POST['ad'];
+  $add->execute();
+}
+ ?>
+
+
 <?php include "menu.php"; ?>
 <h2>Customers</h2>
 <table border="1">
   <tr>
-    <th>First name</th><th>Last name</th><th>Street Address</th>
+    <th>First name</th><th>Last name</th><th>Street Address</th><th>Edit</th>
   </tr>
 
 
 <?php
 include "connection.php";
-$myquery="SELECT firstname,lastname,streetaddress from customers";
+$myquery="SELECT firstname,lastname,streetaddress,id_customers from customers";
 $customers_data=$db->query($myquery);
 foreach ($customers_data as $row) {
   echo '<tr><td>'.
-   $row['firstname'].'</td><td>'.$row['lastname'].'</td><td>'.$row['streetaddress'].'</td></tr>';
+   $row['firstname'].'</td><td>'.$row['lastname'].'</td><td>'.$row['streetaddress'].'</td>';
+  echo '<td><a href="update_customer.php?id='.$row['id_customers'].'"><button>Update</button></a></td>';
+  echo '</tr>';
 }
 
  ?>
---
+++ update_customer.php
@@ -0,0 +1,7 @@
+<?php include "menu.php"; ?>
+<h2>Edit customer</h2>
+<?php
+echo 'the id is '.$_GET['id'];
+ ?>
+
+<?php include "footer.php"; ?>
